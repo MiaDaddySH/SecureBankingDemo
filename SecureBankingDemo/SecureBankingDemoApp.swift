b74253774c4aa983fd17e5daa2431af01fbee452
Yuangang Sheng <martin_sheng@hotmail.com>
2026-06-30 08:30:37 +0200

    chore: create initial SwiftUI iOS project

---
+++ SecureBankingDemo/SecureBankingDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  SecureBankingDemoApp.swift
+//  SecureBankingDemo
+//
+//  Created by Yuangang Sheng on 29.06.26.
+//
+
+import SwiftUI
+
+@main
+struct SecureBankingDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
